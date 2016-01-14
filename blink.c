3980e37abfae5857437a394f9bc146fcbe446f70
schwy <antoine.gueguen@outlook.fr>
2016-01-14 15:42:52 +0100

    added blink_nano for clock nanosleep test

--- blink.c
+++ blink.c
@@ -1,27 +1,28 @@
 /*
- * blink.c:
+ * blink.c: 0 ou 1 sur GPIO17
  */
-
+ 
 #include <stdio.h>
 #include <wiringPi.h>
+#include <time.h>
 
-// LED Pin - wiringPi pin 0 is BCM_GPIO 17.
-
-#define	LED	0
+// PIN - wiringPi pin 0 is BCM_GPIO 17.
+#define	PIN	0
 
 int main(void)
 {
 	printf("Raspberry Pi blink\n");
-
+	int time=0;
 	wiringPiSetup ();
-	pinMode (LED, OUTPUT);
+	pinMode (PIN, OUTPUT);
+	time = clock_gettime;
 
 	for (;;)
 	{
-		digitalWrite(LED, HIGH);	// On
-		delay (200);	// mS
-		digitalWrite(LED, LOW);	// Off
-		delay (200);
+		digitalWrite(PIN, HIGH);	// On
+		//delay (1);	// mS, marche avec 1
+		digitalWrite(PIN, LOW);	// Off
+		//delay (1);	// mS, marche avec 1
 	}
 	return 0;
 }
