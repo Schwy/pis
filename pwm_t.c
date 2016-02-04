4247a3944556e1b2a4714758639b178d2a34c218
schwy <antoine.gueguen@outlook.fr>
2016-02-04 11:52:35 +0000

    modifs pwm

--- pwm_t.c
+++ pwm_t.c
@@ -17,33 +17,33 @@
 #include <wiringPi.h>
 #include <stdio.h>
 
-
+void change_cycle_loop(void);
 int PWMpin= 1;    // 1 right (Or 24 left if model B+).
 int duty = 0;     // Duty cycle
-
+int range = 100; // value passed to pwmSetRange.
 /***************************************************************************
  * menu() function - Display the menu and execute the choices.
  ***************************************************************************/
 void init(void)
 {
-	int range = 100; // value passed to pwmSetRange.
+
 	int clock = 19;   // value passed to pwmSetClock.
 	int mode = 0;     // 0=balanced, 1=mark:space
-	int choice=0;
-	int i; // Index for fade for loop. 
-	int x; // Fade in/out switch.
+
+
+
   
 	//Print information
 	printf("\nDefault pin=\t%d \t\t input?",PWMpin);
-		scanf("%d",&pin);
-		PWMpin = &pin;
+		scanf("%d",&PWMpin);
+		//PWMpin = &PWMpin;
 		pinMode (PWMpin, PWM_OUTPUT);
 	printf("\nDefault range=\t%d \t\t input?",range);
 		scanf("%d", &range);
 		pwmSetRange(range);       // Default 1024 (unsigned int)
-	printf("\nDefault clock=\t%d \t\t input?",range);
+	printf("\nDefault clock=\t%d \t\t input?",clock);
 		scanf("%d", &clock);
-		pwmSetClock(PWMpin, clock);
+		pwmSetClock(clock);
 	printf("\nDefault duty cycle=\t%d \t input?",duty);
 		scanf("%d", &duty);
 		pwmWrite(PWMpin, duty);
@@ -60,12 +60,12 @@ int main(void)
 	return 0;
 }
 
-void change_cycle_loop()
+void change_cycle_loop(void)
 {
 	while(1) // Display the menu.
 	{
 	
-	printf("\nDefault duty cycle=\t%d \t input?",duty);
+	printf("\nCurrent duty cycle=\t%d \t input? [0-%d]",duty,range);
 		scanf("%d", &duty);
 		pwmWrite(PWMpin, duty);
 	}
