0caebb982022b7b3d8a68fca2797cc53917482a8
schwy <antoine.gueguen@outlook.fr>
2016-02-04 14:50:42 +0100

    improved txt

--- pwm_t.c
+++ pwm_t.c
@@ -10,7 +10,7 @@
 
 void change_range_clock_loop(void);
 int PWMpin= 1;    // 1 right (Or 24 left if model B+).
-int duty = 0;     // Duty cycle
+int duty = 25;     // Duty cycle
 int range = 100; // value passed to pwmSetRange.
 int clock = 19;   // value passed to pwmSetClock.
 int mode = 0;     // 0=balanced, 1=mark:space
@@ -18,17 +18,17 @@ int mode = 0;     // 0=balanced, 1=mark:space
 void init(void)
 {
 	//Print information
-	printf("\nDefault pin=\t%d \t\t input? ",PWMpin);
+	printf("\nPin? (default=%d) [1/24]\t",PWMpin);
 		scanf("%d",&PWMpin);
 		//PWMpin = &PWMpin;
 		pinMode (PWMpin, PWM_OUTPUT);
-	printf("\nDefault range=\t%d \t\t input? ",range);
+	printf("\nRange? (default=%d)\t",range);
 		scanf("%d", &range);
 		pwmSetRange(range);       // Default 1024 (unsigned int)
-	printf("\nDefault clock=\t%d \t\t input? ",clock);
+	printf("\nClock? (default=%d)\t",clock);
 		scanf("%d", &clock);
 		pwmSetClock(clock);
-	printf("\nDefault duty cycle=\t%d \t input? ",duty);
+	printf("\nDuty cy.? (default=%d%%)\t",duty);
 		scanf("%d", &duty);
 		pwmWrite(PWMpin, duty);
 }
@@ -47,24 +47,28 @@ int main(void)
 void change_range_clock_loop(void)
 {
 	int mod = 0; // 0=range - 1=clock
-	while(1) // Display choices
+	int duty_r = 0;
+	int clock_r(1) // Display choices
 	{
 		if(mod==0)
 		{
-			pwmWrite(PWMpin, duty);
-			printf("\nCurrent duty cycle=\t%d \t input? [0-%d] (enter range+1 to switch to clock control)",duty,range);
+			pwmWrite(PWMpin, duty_r);
+			printf("\nNew duty cycle? [0-%d] (current=%d%%)\n(range+1 to switch to clock control) ",duty,range);
 			scanf("%d", &duty);
 			if(duty == range+1)
 				{mod++;}
+			else
+				{duty_r=duty;}
 		}
 		if(mod==1)
 		{
-			pwmSetClock(clock);
-			printf("\nCurrent clock=\t%d \t input? (enter 0 to switch to range control)",clock,range);
+			pwmSetClock(clock_r);
+			printf("\nNew clock? (current=%d)\n(0 to switch to range control) ",clock,range);
 			scanf("%d", &clock);
 			if(clock == 0)
 				{mod--;}
-			
+			else
+				{clock_r=clock;}
 		}
 	}
 }
