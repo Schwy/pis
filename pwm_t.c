e6b67af73a8d4577ff9daf847ed21a368c2970cf
schwy <antoine.gueguen@outlook.fr>
2016-02-04 13:01:54 +0100

    switch range/clock

--- pwm_t.c
+++ pwm_t.c
@@ -3,48 +3,32 @@
 
 /***************************************************************************
  * Compile with:  gcc -o pwm_t pwm_t.c.c -lwiringPi
- *
- * arg1 = frequency
- * default = 100Hz
- *
- * arg2 = duty cycle
- * default = 50%
- *
- * arg3 = PIN
- * default = 1 (#6)
  ***************************************************************************/
 
 #include <wiringPi.h>
 #include <stdio.h>
 
-void change_cycle_loop(void);
+void change_range_clock_loop(void);
 int PWMpin= 1;    // 1 right (Or 24 left if model B+).
 int duty = 0;     // Duty cycle
 int range = 100; // value passed to pwmSetRange.
-/***************************************************************************
- * menu() function - Display the menu and execute the choices.
- ***************************************************************************/
+int clock = 19;   // value passed to pwmSetClock.
+int mode = 0;     // 0=balanced, 1=mark:space
+
 void init(void)
 {
-
-	int clock = 19;   // value passed to pwmSetClock.
-	int mode = 0;     // 0=balanced, 1=mark:space
-
-
-
-  
 	//Print information
-	printf("\nDefault pin=\t%d \t\t input?",PWMpin);
+	printf("\nDefault pin=\t%d \t\t input? ",PWMpin);
 		scanf("%d",&PWMpin);
 		//PWMpin = &PWMpin;
 		pinMode (PWMpin, PWM_OUTPUT);
-	printf("\nDefault range=\t%d \t\t input?",range);
+	printf("\nDefault range=\t%d \t\t input? ",range);
 		scanf("%d", &range);
 		pwmSetRange(range);       // Default 1024 (unsigned int)
-	printf("\nDefault clock=\t%d \t\t input?",clock);
+	printf("\nDefault clock=\t%d \t\t input? ",clock);
 		scanf("%d", &clock);
 		pwmSetClock(clock);
-	printf("\nDefault duty cycle=\t%d \t input?",duty);
+	printf("\nDefault duty cycle=\t%d \t input? ",duty);
 		scanf("%d", &duty);
 		pwmWrite(PWMpin, duty);
 }
@@ -56,60 +40,31 @@ int main(void)
 {
 	wiringPiSetup();
 	init();
-	change_cycle_loop();
+	change_range_clock_loop();
 	return 0;
 }
 
-void change_cycle_loop(void)
+void change_range_clock_loop(void)
 {
-	while(1) // Display the menu.
+	int mod = 0; // 0=range - 1=clock
+	while(1) // Display choices
 	{
-	
-	printf("\nCurrent duty cycle=\t%d \t input? [0-%d]",duty,range);
-		scanf("%d", &duty);
-		pwmWrite(PWMpin, duty);
-	}
-}
-	
-/*
-	switch(choice) // Execute the choice.
-	{
-		case 1: // Fade duty cycle within 0 - range - 0.
-			clrscr();
-			x = 1; 
-			for (i=0; i>-1; i+=x)
-			{
-			pwmWrite(PWMpin, i);
-			printf("   %d\n", i);
-			delay (7);
-			if(i > range) x = -1;
-			}
-			pwmWrite(PWMpin, 0);
-			duty=0;
-		break;
-		case 2: // toggle PWM mode Mark:Space/Balanced.
-			mode = !mode;
-			if(mode==0) pwmSetMode(PWM_MODE_BAL); 
-			else pwmSetMode(PWM_MODE_MS); 
+		if(mod==0)
+		{
 			pwmWrite(PWMpin, duty);
-		break;
-		case 3: // Change the range. (number of steps for duty cycle)
-			printf("\n\n\n                          Enter Range: ");
-			scanf("%d", &range);
-			pwmSetRange(range);       // Default 1024 (unsigned int)
-			pwmWrite(PWMpin, duty);
-		break;
-		case 4: // Change the clock.
-			printf("\n\n\n                          Enter Clock: ");
-			scanf("%d", &clock);
-			pwmSetClock(clock);       // Default 32 (int)
-			pwmWrite(PWMpin, duty);
-		break;
-		case 5: // Set the duty cycle.
-			printf("\n\n\n                          Enter Duty Cycle: ");
+			printf("\nCurrent duty cycle=\t%d \t input? [0-%d] (enter range+1 to switch to clock control)",duty,range);
 			scanf("%d", &duty);
-			pwmWrite(PWMpin, duty);
-		break;
+			if(duty == range+1)
+				{mod++;}
+		}
+		if(mod==1)
+		{
+			pwmSetClock(clock);
+			printf("\nCurrent clock=\t%d \t input? (enter 0 to switch to range control)",clock,range);
+			scanf("%d", &clock);
+			if(clock == 0)
+				{mod--;}
+			
 		}
 	}
-	*/
+}
